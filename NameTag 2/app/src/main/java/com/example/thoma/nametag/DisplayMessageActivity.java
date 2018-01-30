3e3bd83ba96c1bfd30e9ec4e358cf740c064e0db
Eric Leong <leong.eric17@gmail.com>
2018-01-30 09:45:34 -0800

    Initial commit

---
+++ NameTag 2/app/src/main/java/com/example/thoma/nametag/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.thoma.nametag;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Login.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        //TextView textView = (TextView) findViewById(R.id.textView);
+    }
+    /** Called when the user taps the Send button */
+    public void nextActivity(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
